890a36a117fbd4498f02606f17b9f181fd81f797
Mike Fullerton <mike@greentongue.com>
2013-12-24 22:10:40 -0800

    clean up

--- FishLampTestApps/FishLampTestApps/main.m
+++ FishLampTestApps/FishLampTestApps/main.m
@@ -7,12 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "FishLampCore.h"
+
+
+#define ASDF(BOB, FOO...) do  { \
+        NSLog(BOB); \
+        NSString* s = [NSString stringWithFormat:@"" FOO]; \
+        NSLog(s); \
+    } \
+    while(0)
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
+
+    id object = nil;
+
+    ASDF(@"foo");
+    ASDF(@"foo", @"hi");
+    ASDF(@"foo", @"hi %@", @"mike");
+
+//    FLAssertionFailed();
+
+    FLAssert(1 == 0, @"wth");
+    FLAssert(1 == 0);
+
+//    FLAssertNotNil(object);
+
+
+
+//        FLAssertionFailed(@"Hello %@", @"this is mike");
+
         // insert code here...
         NSLog(@"Hello, World!");
         
